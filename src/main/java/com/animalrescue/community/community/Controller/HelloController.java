932b24ea2552988ffb0927a2fef99bbd943e7226
= <3115685335@qq.com>
2022-02-19 14:04:45 +0800

    first commit

--- src/main/java/com/animalrescue/community/community/Controller/HelloController.java
+++ src/main/java/com/animalrescue/community/community/Controller/HelloController.java
@@ -1,4 +1,16 @@
 package com.animalrescue.community.community.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller //controller 允许这个类接受前端的请求
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
 }
